ef604a44cf9af804cd0bc8e9b4c35c7d970ff461
Alyssa Iseppi <53068550+aiseppi@users.noreply.github.com>
2022-06-08 13:24:33 -0400

    header update

--- src/components/Sweets/SweetsSummary.js
+++ src/components/Sweets/SweetsSummary.js
@@ -5,9 +5,9 @@ const SweetsSummary = () => {
     <section className={classes.summary}>
       <h2>ALL NATURAL INGREDIENTS</h2>
       <p>
-        Here at Violet's Sweet Treats all of our goods are made with
-        high-quality, natural, and locally-sourced ingredients. All of our
-        delicious items are crafted fresh from scratch ever day in our bakery.
+        Here at Violet's Sweet Treats all of our delicious items are crafted
+        fresh from scratch every day with high-quality, natural, and
+        locally-sourced ingredients.
       </p>
       <p>
         Choose below from our broad selection of available sweets and enjoy a
